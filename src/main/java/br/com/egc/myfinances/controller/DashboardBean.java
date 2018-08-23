175e5345788508a661d776ff1035b0d00b3312a5
ecarvalho <erasmo.carvalho@lello.com.br>
2018-08-23 16:57:05 -0300

    alterando icones dashboard

--- src/main/java/br/com/egc/myfinances/controller/DashboardBean.java
+++ src/main/java/br/com/egc/myfinances/controller/DashboardBean.java
@@ -6,23 +6,14 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.primefaces.model.chart.Axis;
-import org.primefaces.model.chart.AxisType;
-import org.primefaces.model.chart.CategoryAxis;
-import org.primefaces.model.chart.LineChartModel;
-import org.primefaces.model.chart.LineChartSeries;
-
 import br.com.egc.myfinances.entity.ContaVO;
-import br.com.egc.myfinances.service.ContaService;
 import br.com.egc.myfinances.service.DashboardService;
 import br.com.egc.myfinances.service.Util;
 import lombok.Getter;
-import lombok.Setter;
 
 @SessionScoped
 @Named
@@ -31,9 +22,7 @@ public class DashboardBean extends BaseBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Getter
-	@Setter
 	private ContaVO contaVO;
-
 	@Getter
 	private BigDecimal saldoAtual;
 	@Getter
@@ -42,53 +31,42 @@ public class DashboardBean extends BaseBean implements Serializable {
 	private BigDecimal totalRendas;
 	@Getter
 	private BigDecimal balanco;
-	
-	@Getter
-	@Setter
-	private LineChartModel areaModel;
 
 	@Inject
 	private DashboardService dashboardService;
 
 	public void initDashboard() {
 
-		List<ContaVO> listContaVO =  dashboardService.listarConta();
-		
-		if(!listContaVO.isEmpty()) {
-			
+		List<ContaVO> listContaVO = dashboardService.listarConta();
+
+		if (!listContaVO.isEmpty()) {
+
 			contaVO = dashboardService.listarConta().get(0);
-			
+
 			saldoAtual = contaVO.getSaldoAtual();
-			
+
 			String mesAnoSelecionado;
 			Calendar calendarAtual;
 			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-YYYY");
-			
+
 			calendarAtual = Calendar.getInstance();
-			
+
 			mesAnoSelecionado = simpleDateFormat.format(calendarAtual.getTime());
-			
+
 			totalDespesas = dashboardService.buscarTotalDespesas(mesAnoSelecionado);
 			totalRendas = dashboardService.buscarTotalRendas(mesAnoSelecionado);
 			balanco = BigDecimal.ZERO;
 			balanco = totalRendas.add(totalDespesas);
-			
-			Util.setContaNaSession(contaVO);
-			
-			createAreaModel();
 
-		}else {
-			
+			Util.setContaNaSession(contaVO);
+
+		} else {
+
 			saldoAtual = BigDecimal.ZERO;
 			totalDespesas = BigDecimal.ZERO;
 			totalRendas = BigDecimal.ZERO;
 			balanco = BigDecimal.ZERO;
 		}
-		
-		
-
-		
-
 
 	}
 
@@ -97,45 +75,5 @@ public class DashboardBean extends BaseBean implements Serializable {
 		redirect("dashboard.xhtml");
 
 	}
-	
-	
-	
-	
-	private void createAreaModel() {
-        areaModel = new LineChartModel();
- 
-        LineChartSeries boys = new LineChartSeries();
-        boys.setFill(true);
-        boys.setLabel("Boys");
-        boys.set("2004", 120);
-        boys.set("2005", 100);
-        boys.set("2006", 44);
-        boys.set("2007", 150);
-        boys.set("2008", 25);
- 
-        LineChartSeries girls = new LineChartSeries();
-        girls.setFill(true);
-        girls.setLabel("Girls");
-        girls.set("2004", 52);
-        girls.set("2005", 60);
-        girls.set("2006", 110);
-        girls.set("2007", 90);
-        girls.set("2008", 120);
- 
-        areaModel.addSeries(boys);
-        areaModel.addSeries(girls);
- 
-        areaModel.setTitle("Area Chart");
-        areaModel.setLegendPosition("ne");
-        areaModel.setStacked(true);
-        areaModel.setShowPointLabels(true);
- 
-        Axis xAxis = new CategoryAxis("Years");
-        areaModel.getAxes().put(AxisType.X, xAxis);
-        Axis yAxis = areaModel.getAxis(AxisType.Y);
-        yAxis.setLabel("Births");
-        yAxis.setMin(0);
-        yAxis.setMax(300);
-    }
 
 }
